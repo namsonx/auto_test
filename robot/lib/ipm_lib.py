b2718ad3405b9d052ddbe55df84f7f222dd6ed7b
MAS2HC <MAS2HC@HC-UT40281N.hc.apac.bosch.com>
2017-07-09 20:49:30 +0530

    Creating ipm_lib file

---
+++ robot/lib/ipm_lib.py
@@ -0,0 +1,8 @@
+'''
+This module is define libs for ipm
+author: Son Mai
+'''
+
+import os
+import sys
+import lib.objects
--- robot/lib/objects.py
+++ robot/lib/objects.py
@@ -1,4 +1,4 @@
-#This module defines the libs for parking place 
+#This module defines the objects for parking place 
 #Author: Son Mai
 
 import sys
@@ -25,3 +25,22 @@ class parking_place(object):
         ser_ip = server_ip
         ser_port = port
         
+class parking_lot(object):
+    '''
+    This is base class for parking lot
+    '''
+    def __init__(self, lot_id, place_id, **kwargs):
+        self.id = lot_id
+        self.place = place_id
+        self.name = ''
+        self.unicode = ''
+        self.createby = ''
+        self.availability = 0
+        
+class parking_block(object):
+    '''
+    This is base class for parking block
+    '''
+    def __init__(self, block_id, place_id, lot_id = None, **kwargs):
+        self.id = block_id
+        self.place = place_id
