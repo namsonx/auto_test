635938f9c975e65dea4c457cba23997de4678113
MAS2HC <MAS2HC@HC-UT40281N.hc.apac.bosch.com>
2017-07-10 20:59:33 +0530

    Updating the lib module

--- robot/lib/ipm_lib.py
+++ robot/lib/ipm_lib.py
@@ -7,3 +7,26 @@ import os
 import sys
 import lib.objects
 
+
+def get_place_info(place_id = None, place_name = ''):
+    print 'Starting get place infog'
+    
+def create_parking_place(place_name, unicode, parktype = 0, **kwargs):
+    if kwargs.get('passrule'):
+        passrule = kwargs.passrule
+    else:
+        passrule = '24hr'
+    
+    if kwargs.get('createby'):
+        createby = kwargs.createby
+    else:
+        createby = 'Admin'
+    
+    if kwargs.get('longitude') and kwargs.get('latitude'):
+        long = kwargs.longtitude
+        lat = kwargs.latitude
+    else:
+        long = 77.61259979044553
+        lat = 12.935567399778256
+        
+    
